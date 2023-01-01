12ea199f4689ca34d29f531623230b2bcaafef3e
jbenguira <z51biz@gmail.com>
2023-01-01 15:55:26 +0200

    fixed permissions

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -3,6 +3,8 @@ set -o allexport; source .env; set +o allexport;
 
 
 mkdir zammad_backup zammad_data elasticsearch_data postgresql_data
-chown -R 1000:0 elasticsearch_data
+chown -R 1001:1001 elasticsearch_data
 chown -R 1000:1000 zammad_data
 chown -R 1000:1000 zammad_backup
+
+chmod +x scripts/backup.sh
