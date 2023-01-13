b0d9e3f9df2a1012986d178c57f14d087510921c
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-01-13 13:17:13 +0530

    update post deploy.sh script for domain

--- scripts/postDeploy.sh
+++ scripts/postDeploy.sh
@@ -2,5 +2,5 @@
 # set -o allexport; source .env; set +o allexport;
 
 #fix SSL Reverse proxy for zammad
-sed -i '/^proxy_set_header X-Forwarded-Proto.*/a proxy_set_header X-Forwarded-Ssl on;' /opt/elestio/nginx/conf.d/${CI_CD_DOMAIN}.conf
+sed -i '/^proxy_set_header X-Forwarded-Proto.*/a proxy_set_header X-Forwarded-Ssl on;' /opt/elestio/nginx/conf.d/${DOMAIN}.conf
 docker exec elestio-nginx nginx -s reload;
